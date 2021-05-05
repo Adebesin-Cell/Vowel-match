34ff11be32f31458f57cca79702ee6584833ccd2
Adebesin-Cell <adebesintolulope80@gmail.com>
2021-05-05 11:24:24 +0100

    code updated

---
+++ script.js
@@ -0,0 +1,35 @@
+const vowels = ["a", "e", "i", "o", "u"];
+let count = 0;
+console.log();
+
+//Using a nested for loop and strict equality
+function match(str) {
+  let stringLength = str.length;
+  for (let i = 0; i < stringLength; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (str[i] === vowels[j]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+console.log(match("console"));
+
+//Using a nested for loop and Js .includes() method
+let count2 = 0;
+function match2(str) {
+  let stringLength = str.length;
+  for (let i = 0; i < stringLength; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (str[i].includes(vowels[j])) {
+        count2++;
+      }
+    }
+  }
+  return count2;
+}
+
+//console.log(match2("BisolaMeDun"));
+
+//
